a31d6eb11f0f17603c88b9469d1e825822c5306a
Guest User <Guest@Epicodus-9C.local>
2017-11-03 12:55:57 -0700

    Add functionality to gather user choices and return an order price

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,42 @@
 var numberToppings;
+var toppingQuantity;
+var size;
+var toppingsCost;
 
-function Pizza(topping, size) {
-  this.toppings = toppings;
+function Pizza(size, numberToppings) {
+  this.numberToppings = numberToppings;
   this.size = size;
+  this.toppingsCost = toppingsCost;
   this.price;
   this.setPizzaPrice();
 }
 
 Pizza.prototype.yourPizzaOrder = function(){
+  return this.size + ", " + this.price;
 }
 
 Pizza.prototype.setPizzaPrice = function() {
-  var pizzaCost = 10 * value from size
-  var toppingsCost = number of toppings selected * 2
-return pizzaCost + toppingsCost
-
-
+  var numberToppings = ($("input:checkbox:checked").length);
+  if (this.size === "small") {
+    return this.price = 10 + (numberToppings * 2);
+  } else if (this.size === "medium") {
+    return this.price = 15 + (numberToppings * 2);
+  } else if (this.size === "large") {
+    return this.price = 20 + (numberToppings * 2);
+  } else if (this.size === "gigantic") {
+    return this.price = 30 + (numberToppings * 2);
+  }
 }
-
 
 //Interface Logic goes here
 $(document).ready(function() {
   $("#button").click(function(event) {
     event.preventDefault();
     debugger;
-    var numberToppings = $('input[type="checkbox"]:checked').length;
 
+    var size = $("select#size").val();
+    var pizzaOrder = new Pizza(size, numberToppings);
+    $("ul#yourOrder").append("<li>" + pizzaOrder.yourPizzaOrder() + "</span></li>");
 
   }); //submit event
 });//document ready

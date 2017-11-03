717a27205f0fdc8386e21c8948c30e8691d49807
Guest User <Guest@Epicodus-9C.local>
2017-11-03 10:31:05 -0700

    Add function to count toppings, begin creating logic for pizza price

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,31 @@
+var numberToppings;
+
 function Pizza(topping, size) {
-  this.topping = topping;
+  this.toppings = toppings;
   this.size = size;
   this.price;
-  this.setPrice();
+  this.setPizzaPrice();
 }
 
 Pizza.prototype.yourPizzaOrder = function(){
+}
+
+Pizza.prototype.setPizzaPrice = function() {
+  var pizzaCost = 10 * value from size
+  var toppingsCost = number of toppings selected * 2
+return pizzaCost + toppingsCost
+
 
 }
 
-Ticket.prototype.setPizzaPrice = function() {
+
+//Interface Logic goes here
+$(document).ready(function() {
+  $("#button").click(function(event) {
+    event.preventDefault();
+    debugger;
+    var numberToppings = $('input[type="checkbox"]:checked').length;
 
 
-}
+  }); //submit event
+});//document ready

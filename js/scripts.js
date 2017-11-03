be2beba362cf72d6336d98217260f1430d1e92e0
Guest User <Guest@Epicodus-9C.local>
2017-11-03 09:01:42 -0700

    Add placeholders for Pizza constructor and prototypes and framework for order form

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+function Pizza(topping, size) {
+  this.topping = topping;
+  this.size = size;
+  this.price;
+  this.setPrice();
+}
+
+Pizza.prototype.yourPizzaOrder = function(){
+
+}
+
+Ticket.prototype.setPizzaPrice = function() {
+
+
+}
